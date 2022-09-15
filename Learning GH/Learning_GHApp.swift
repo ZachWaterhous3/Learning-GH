552e7e0dace9302ba7f4ad2b0f407f3582d1a300
Zach Waterhouse <zacwaterhouse98@gmail.com>
2022-09-15 18:50:20 +1000

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by Zach Waterhouse on 15/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
